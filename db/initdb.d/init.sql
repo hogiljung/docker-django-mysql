8c4f33d7b547e63cbcdee21b32eee3fe0076274c
hogiljung <112647829+hogiljung@users.noreply.github.com>
2023-02-12 00:54:15 +0900

    Add comment table, init data.

--- db/initdb.d/init.sql
+++ db/initdb.d/init.sql
@@ -41,3 +41,16 @@ CREATE TABLE post_content
     FOREIGN KEY(post_id) REFERENCES post(id) ON DELETE CASCADE,
     PRIMARY KEY(post_id)
 );
+
+CREATE TABLE comment
+(
+    id              INT UNSIGNED NOT NULL,
+    content         TEXT,
+    created_date    DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_date    DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    post_id         INT            UNSIGNED NOT NULL,
+    user_id         CHAR(36)       NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES user(id),
+    FOREIGN KEY(post_id) REFERENCES post(id) ON DELETE CASCADE,
+    PRIMARY KEY(id, post_id)
+);
--- db/initdb.d/load_data.sql
+++ db/initdb.d/load_data.sql
@@ -5,3 +5,5 @@ INSERT INTO user(id, username, password) VALUES ("3f06af63-a93c-11e4-9797-005056
 INSERT INTO post(title, brief_description, user_id) VALUES("hi", "Great view!", "3f06af63-a93c-11e4-9797-00505690773f");
 
 INSERT INTO post_content(post_id, content, large_content) VALUES(1, "Greate view! Have a nice day.", "0x89504E470D0A1A0A");
+
+INSERT INTO comment(id, content, user_id, post_id) VALUES(1, "So Amazing!", "3f06af63-a93c-11e4-9797-00505690773f", 1);
